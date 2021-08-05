14dd46402717ff4adb5ec3fb6c28b11a5b6e3720
Kangjun Heo == P.Knowledge <knowledge@o.cnu.ac.kr>
2021-08-05 17:53:19 +0900

    simple example for windows-rs

--- Sources/src/main.rs
+++ Sources/src/main.rs
@@ -1,3 +1,18 @@
-fn main() {
-    println!("Hello, world!");
+mod bindings {
+    windows::include_bindings!();
+}
+
+use bindings::{
+    Windows::Win32::UI::WindowsAndMessaging::{MessageBoxW, MB_OK}
+};
+
+fn main() -> windows::Result<()> {
+    unsafe {
+        MessageBoxW(None, 
+            "Hello, World!", 
+            "Windows Rust Example", 
+            MB_OK);
+    }
+
+    Ok(())
 }
